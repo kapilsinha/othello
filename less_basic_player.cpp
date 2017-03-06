7de6bf62cc63a85581b84bc21fe71f49d37618ea
Kapil Sinha <ksinha@caltech.edu>
2017-03-06 06:26:10 -0800

    Added methods to the board class. Also added/updated a few functions in the less_basic_player class. Some revisions may not have been actual improvements, but both versions are still in the code (with the older version commented out). It is still very much in progress and has not been tested.

--- less_basic_player.cpp
+++ less_basic_player.cpp
@@ -24,7 +24,37 @@ Player::~Player() {
 /*
  * Checks if we can place a token on the "power spots"
  */
-Move *Player::checkPowerSpots(Move *opponentsMove)
+Move *Player::checkPowerSpots(vector< tuple<int, int> > move_coords)
+// this actually may be slower/worse than the original version
+{ 
+    tuple<int, int> powerspots[16];
+    powerspots[0] = tuple<int, int> (0, 0);
+    powerspots[1] = tuple<int, int> (7, 7);
+    powerspots[2] = tuple<int, int> (0, 7);
+    powerspots[3] = tuple<int, int> (7, 0);
+    powerspots[4] = tuple<int, int> (0, 2);
+    powerspots[5] = tuple<int, int> (0, 5);
+    powerspots[6] = tuple<int, int> (2, 0);
+    powerspots[7] = tuple<int, int> (2, 5);
+    powerspots[8] = tuple<int, int> (5, 0);
+    powerspots[9] = tuple<int, int> (5, 7);
+    powerspots[10] = tuple<int, int> (7, 2);
+    powerspots[11] = tuple<int, int> (7, 5);
+    powerspots[12] = tuple<int, int> (2, 2);
+    powerspots[13] = tuple<int, int> (2, 5);
+    powerspots[14] = tuple<int, int> (5, 2);
+    powerspots[15] = tuple<int, int> (5, 5);
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < move_coords.size(); j++) {
+            if (get<0>(move_coords[i]) == get<0>(powerspots[i])
+             && get<1>(move_coords[i]) == get<1>(powerspots[i])) {
+                return new Move(get<0>(move_coords[i]), get<1>(move_coords[i]));
+            }
+        }
+    }
+}
+/* The following is the older version but may actually be better/faster
+Move *Player::checkPowerSpots()
 {
     Move* powerspots[] = new Move*[16];
     powerspots[0] = new Move(0, 0);
@@ -55,6 +85,37 @@ Move *Player::checkPowerSpots(Move *opponentsMove)
     }
     return nullptr;
 }
+*/
+
+/*
+ * Picks the move that maximizes the number of moves next turn
+ * May need to account for all possible moves from opponent
+ * (maybe this will be implemented in minimax)
+ */
+Move *Player::maximizeMoves(Board * simulated_game,
+                            vector< tuple<int, int> > move_coords,
+                            Side side)
+{ 
+    int max_num_moves = 0;
+    int num_moves;
+    Move * best_move = nullptr;
+    for (int i = 0; i < move_coords.size(); i++) {
+        Move * move = new Move(get<0>(move_coords[i]), get<1>(move_coords[i]));
+        simulated_game.doMove(move, side);
+        num_moves = game.numMoves(side);
+        if (num_moves > max_num_moves) {
+            max_num_moves = num_moves;
+            best_move = move;
+        }
+        // WE NEED TO ADD AN UNDO MOVE METHOD TO BOARD
+        // I AM PRETENDING THAT THIS METHOD EXISTS FOR NOW BUT IT DOES NOT!
+        // IT WILL REQUIRE US TO ADD A VARIABLE/ARRAY last_move(s) TO BOARD
+        // Add this function to doMove() when the above works with undoMove()
+        simulated_game.undoMove();
+        delete move;
+    }
+    return best_move;
+}
 
 /*
  * Compute the next move given the opponent's last move. Your AI is
@@ -79,12 +140,22 @@ Move *Player::doMove(Move *opponentsMove, int msLeft)
     }
 
     game.doMove(opponentsMove, (Side) (1 - side));
+    Board * simulated_game = game.copy();
 
     // not-so-intelligent AI: returns the first move found
-    Move *move = checkPowerSpots(*opponentsMove);
     if (move != nullptr) {
         return move;
     }
+    Move* [] moves;
+    vector< tuple<int, int> > move_coords = game.getMoves(side);
+    Move *move = checkPowerSpots(move_coords);
+    for (int i = 0; i < move_coords.size(); i++) {
+        Move* move = new Move(get<0>(move_coords[i]), get<1>(move_coords[i]));
+        game.doMove(move, side);
+        return move;
+    }
+    
+    /**
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             Move* move = new Move(i, j);
@@ -97,5 +168,6 @@ Move *Player::doMove(Move *opponentsMove, int msLeft)
             }
         }
     }
+    */
     return nullptr;	
 }
